1a924bdc980a19bf4d8c1790fa0c877ac63b3c98
belapko <belapko23@gmail.com>
2023-03-22 20:48:43 +0300

    created products list and added some setting to backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,50 @@
 import './App.css';
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Products from "./components/Products";
+import axios from "axios";
+
+const productsApiUrl = "http://127.0.0.1:8000/api/products/"
 
 function App() {
-    return (
-        <div className="wrapper">
-            <BrowserRouter>
-                <Header/>
-                <Routes>
-                    <Route/>
-                </Routes>
-                <Footer/>
-            </BrowserRouter>
-        </div>
-    );
+    const [error, setError] = useState(null);
+    const [products, setProducts] = useState([]);
+    const [isLoaded, setIsLoaded] = useState(false)
+
+    useEffect(() => {
+        fetch(productsApiUrl)
+            .then(response => response.json())
+            .then(
+                (result) => {
+                    setIsLoaded(true);
+                    setProducts(result);
+                },
+                (error) => {
+                    setIsLoaded(true);
+                    setError(error);
+                }
+            )
+    }, [])
+
+    if (error) {
+        return <div>Ошибка: {error.message}</div>;
+    } else if (!isLoaded) {
+        return <div>Загрузка...</div>;
+    } else {
+        return (
+            <div className="wrapper">
+                <BrowserRouter>
+                    <Header/>
+                    <Routes>
+                        <Route path="/" element={<Products products={products}/>}/>
+                    </Routes>
+                    <Footer/>
+                </BrowserRouter>
+            </div>
+        );
+    }
 }
 
-export default App;
+    export default App;
