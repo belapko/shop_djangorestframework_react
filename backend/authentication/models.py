754e522429e867c67def9fbe4e1bb8cacaaea1ab
belapko <belapko23@gmail.com>
2023-03-24 00:52:58 +0300

    created authentication with email confirmation at backend

---
+++ backend/authentication/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+
+
+class UserManager(BaseUserManager):
+    def _create_user(self, username=None, email=None, password=None, **extra_fields):
+        if not username:
+            email = self.normalize_email(email)
+            username = email
+        user = self.model(email=email, username=username, **extra_fields)
+
+        if extra_fields.get('is_superuser'):
+            user = self.model(username=username, **extra_fields)
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, username, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(username=username, email=email, password=password, **extra_fields)
+
+    def create_superuser(self, username, password, **extra_fields):
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_active', True)
+        extra_fields.setdefault('is_verified', True)
+
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(username=username, password=password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(max_length=100, unique=True, blank=True)
+    email = models.EmailField(null=False, blank=False, unique=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_verified = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
